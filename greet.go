f1e9571fe448bd5aa5f552d089c14e85f0ebd1af
Toni Kusnandar <toni@kusnandar.dev>
2022-11-07 10:35:33 +0700

    add person

--- greet.go
+++ greet.go
@@ -2,6 +2,42 @@ package greet
 
 import "fmt"
 
+type person struct {
+	id    int
+	name  string
+	hobby string
+}
+
+func NewPerson() person {
+	return person{}
+}
+
+func (p person) SetId(id int) {
+	p.id = id
+}
+func (p person) SetName(name string) {
+	p.name = name
+}
+func (p person) SetHobby(hobby string) {
+	p.hobby = hobby
+}
+
+func (p person) GetId() int {
+	return p.id
+}
+
+func (p person) GetName() string {
+	return p.name
+}
+
+func (p person) GetHobby() string {
+	return p.hobby
+}
+
+func (p person) ToString() string {
+	return fmt.Sprintf("Halo, nama saya %s, hobi saya adalah %s", p.name, p.hobby)
+}
+
 func HelloFromToni() {
 	fmt.Println("Hai Semua, ini terupdate!")
 }
